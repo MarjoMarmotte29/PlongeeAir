8cbd026e75b838cf58ef19fb41c10f3b1eeae692
MarjoMarmotte29 <mjogaret@yahoo.fr>
2017-09-29 18:50:47 +0200

    essai mise en forme titre

--- App.js
+++ App.js
@@ -2,13 +2,29 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+  async componentDidMount() {
+    await Font.loadAsync({
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Calcul de Décompression Plongées MN92</Text>
+        {
+    this.state.fontLoaded ? (
+      <Text style={styles.Text}>Calcul de Décompression Plongées MN92</Text>
+    ) : null
+  }
+       
       </View>
     );
-  }
+  
+}
 }
 
 const styles = StyleSheet.create({
@@ -16,7 +32,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'blue',
     alignItems: 'center',
-    justifyContent: 'center',
-    color: "#fff",
+    
   },
+  Text :{
+    fontFamily: 'open-sans-old',
+    fontSize: 30,
+    color: '#fff',
+    marginTop:150,
+  }
 });
